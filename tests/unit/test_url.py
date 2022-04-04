84040dabf9ca139c1f14e847a9defdb117117f67
Guillaume Fauvel <guillaume.fauvelpro@pro.com>
2022-04-04 14:15:20 +0200

    Ajout des tests URLs

--- tests/unit/test_url.py
+++ tests/unit/test_url.py
@@ -9,7 +9,6 @@ from server import create_app
 
 @pytest.fixture
 def client():
-
     app = create_app()
     with app.test_client() as client:
         yield client
@@ -46,3 +45,32 @@ def test_summary_without_login(client):
     rv = client.get("/showSummary", follow_redirects=True)
 
     assert rv.status_code == 405
+
+
+def _competitions_assigment(client, selected_competition, selected_club, placesRequired, expected_msg, expected_url):
+
+    rv = client.post("/purchasePlaces", data=dict(competition=selected_competition,
+                                                club=selected_club,
+                                                places=placesRequired), 
+                    follow_redirects=True)
+
+    url = "".join((flask.request.url).split("/")[3:])
+
+    assert url == expected_url
+    assert rv.status_code == 200
+    assert rv.data.decode().find(expected_msg) != -1
+
+
+def test_without_credits(client):
+    
+    _competitions_assigment(client, "Fall Classic", "Iron Temple", 5, "You don't have enough points", "bookFall%20ClassicIron%20Temple")
+    
+
+def test_with_credits(client):
+    
+    _competitions_assigment(client, "Fall Classic", "Iron Temple", 1, "Great-booking complete!", "purchasePlaces")
+
+
+def test_without_credit_spending(client):
+    
+    _competitions_assigment(client, "Fall Classic", "Iron Temple", 0, " ", "purchasePlaces")
